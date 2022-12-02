e99af14bea3265a8f2a14673814bbd55ab13513e
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-03 02:49:13 +0300

    Refactored App Function

--- Soar/client/src/App.js
+++ Soar/client/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect } from "react";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch("/movies")
+      .then((r) => r.json())
+      .then((movies) => console.log(movies));
+  }, []);
+
+  return <h1>Hello from React!</h1>;
 }
 
 export default App;
+
